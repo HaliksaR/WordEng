400167e8a9c03c8f9c0ba650522a22b5930cba65
Артем Скоркин <artemka.skorkin@gmail.com>
2018-04-24 22:36:43 +0700

    Add console graphics

--- src/console/logo.c
+++ src/console/logo.c
@@ -1,48 +1,57 @@
 #include <stdio.h>
 #include <time.h>
+#include "includes/logo.h"
 
-
-    #define RESET "\033[0m"
-    #define GREEN "\033[1;32m"
-    #define RED   "\033[1;31m"
-    #define CYAN  "\033[1;36m"
-    #define GRAY  "\033[2m"
-    #define WHITE  "\033[37m"
-    #define BLACK "\033[0;30m" 
-    // https://habrahabr.ru/post/119436/
-
-void delay()
+void delay_logo()
 {
     float milli_seconds = 40000;
     float start_time = clock();
     while (clock() <= start_time + milli_seconds);
 }
 
-
-int main () {
-	delay();
-    printf("%s\t██████████████████████████████\n" , BLACK); delay();
-    printf("\t████████%s" "███%s" "███████████████████\n", RED, BLACK); delay();
-    printf("\t█████%s" "██%s" "███%s" "█████%s" "███████████████\n", RED, BLACK, RED, BLACK); delay();
-    printf("\t███████%s" "█████████████%s" "██████████%s\n", RED, BLACK, RED); delay();
-    printf("\t████%s" "███%s" "█████████████████%s" "██████\n", BLACK,RED, BLACK); delay();
-    printf("\t█████%s" "█████%s" "██%s" "███████%s" "██████████%s" "█\n", RED, WHITE, RED, WHITE, BLACK); delay();
-    printf("\t█████%s" "█████%s" "██%s" "█████%s" "██%s" "██%s" "██%s" "████%s" "███%s\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK, RED); delay();
-    printf("\t██%s" "█%s" "██████%s" "██%s" "█████%s" "██%s" "██%s" "██%s" "██████%s" "██\n", BLACK, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay();
-    printf("\t███%s" "█████%s" "██%s" "██%s" "██%s" "█%s" "██%s" "██%s" "██████%s" "███%s" "██\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay();
-    printf("\t█%s" "███████%s" "█%s" "██%s" "██%s" "██%s" "█%s" "██%s" "██%s" "████████%s" "██\n",  RED, WHITE, RED, WHITE, RED, WHITE , RED, WHITE, RED, BLACK); delay();
-    printf("\t█%s" "██████%s" "██%s" "█%s" "██%s" "██%s" "█%s" "██%s" "██%s" "█████████%s" "██\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay();
-    printf("\t███%s" "████%s" "█%s" "█%s" "█%s" "█%s" "█%s" "█%s" "█%s" "██%s" "██%s" "██%s" "███%s" "█████%s" "██\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay();
-    printf("\t████%s" "███%s" "██%s" "██%s" "███%s" "██%s" "████%s" "███████%s" "███\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay();
-    printf("\t█████%s" "██%s" "█%s" "██████████████████%s" "████\n", RED, WHITE, RED, BLACK); delay();
-    printf("\t███████%s" "██████████████████%s" "█████\n", RED, BLACK); delay();
-    printf("\t██████████%s" "███████████%s" "█████████%s\n", RED, BLACK, RESET); delay();
-    printf ("%s\n _       __\t\t  __   ______\n",RED); delay();     
-    printf ("| |     / /___  _________/ /  / ____/___  ____ _\n" ); delay(); 
-    printf ("| | /| / / __ \\/ ___/ __  /  / __/ / __ \\/ __ `/\n" ); delay(); 
-    printf ("| |/ |/ / /_/ / /  / /_/ /  / /___/ / / / /_/ /\n" ); delay(); 
-    printf ("|__/|__/\\____/_/   \\__,_/  /_____/_/ /_/\\__, /\n" ); delay(); 
-    printf ("\t\t\t\t       /____/\n%s", RESET);
-	main();
-  	return 0;
+void logo() {
+    gotoxy(12,5);    
+    printf("%s██████████████████████████████\n" , BLACK); delay_logo(40000);
+    gotoxy(12,6);   
+    printf("████████%s" "███%s" "███████████████████\n", RED, BLACK); delay_logo(40000);
+    gotoxy(12,7);   
+    printf("█████%s" "██%s" "███%s" "█████%s" "███████████████\n", RED, BLACK, RED, BLACK); delay_logo(40000);
+    gotoxy(12,8);   
+    printf("███████%s" "█████████████%s" "██████████%s\n", RED, BLACK, RED); delay_logo(40000);
+    gotoxy(12,9);   
+    printf("████%s" "███%s" "█████████████████%s" "██████\n", BLACK,RED, BLACK); delay_logo(40000);
+    gotoxy(12,10);   
+    printf("█████%s" "█████%s" "██%s" "███████%s" "██████████%s" "█\n", RED, WHITE, RED, WHITE, BLACK); delay_logo(40000);
+    gotoxy(12,11);   
+    printf("█████%s" "█████%s" "██%s" "█████%s" "██%s" "██%s" "██%s" "████%s" "███%s\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK, RED); delay_logo(40000);
+    gotoxy(12,12);   
+    printf("██%s" "█%s" "██████%s" "██%s" "█████%s" "██%s" "██%s" "██%s" "██████%s" "██\n", BLACK, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay_logo(40000);
+    gotoxy(12,13);   
+    printf("███%s" "█████%s" "██%s" "██%s" "██%s" "█%s" "██%s" "██%s" "██████%s" "███%s" "██\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay_logo(40000);
+    gotoxy(12,14);   
+    printf("█%s" "███████%s" "█%s" "██%s" "██%s" "██%s" "█%s" "██%s" "██%s" "████████%s" "██\n",  RED, WHITE, RED, WHITE, RED, WHITE , RED, WHITE, RED, BLACK); delay_logo(40000);
+    gotoxy(12,15);   
+    printf("█%s" "██████%s" "██%s" "█%s" "██%s" "██%s" "█%s" "██%s" "██%s" "█████████%s" "██\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay_logo(40000);
+    gotoxy(12,16);   
+    printf("███%s" "████%s" "█%s" "█%s" "█%s" "█%s" "█%s" "█%s" "█%s" "██%s" "██%s" "██%s" "███%s" "█████%s" "██\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay_logo(40000);
+    gotoxy(12,17);   
+    printf("████%s" "███%s" "██%s" "██%s" "███%s" "██%s" "████%s" "███████%s" "███\n", RED, WHITE, RED, WHITE, RED, WHITE, RED, BLACK); delay_logo(40000);
+    gotoxy(12,18);   
+    printf("█████%s" "██%s" "█%s" "██████████████████%s" "████\n", RED, WHITE, RED, BLACK); delay_logo(40000);
+    gotoxy(12,19);   
+    printf("███████%s" "██████████████████%s" "█████\n", RED, BLACK); delay_logo(40000);
+    gotoxy(12,20);   
+    printf("██████████%s" "███████████%s" "█████████%s\n", RED, BLACK, RESET); delay_logo(40000);
+    gotoxy(5,22);   
+    printf ("%s_       __\t\t     __   ______\n",RED); delay_logo(40000);    
+    gotoxy(4,23);  
+    printf ("| |     / /___  _________/ /  / ____/___  ____ _\n" ); delay_logo(40000); 
+    gotoxy(4,24); 
+    printf ("| | /| / / __ \\/ ___/ __  /  / __/ / __ \\/ __ `/\n" ); delay_logo(40000); 
+    gotoxy(4,25); 
+    printf ("| |/ |/ / /_/ / /  / /_/ /  / /___/ / / / /_/ /\n" ); delay_logo(40000); 
+    gotoxy(4,26); 
+    printf ("|__/|__/\\____/_/   \\__,_/  /_____/_/ /_/\\__, /\n" ); delay_logo(40000); 
+    gotoxy(43,27); 
+    printf ("/____/\n%s", RESET);
 }
