61ea99042f16cff014761a796f107932d691c00a
HaliksaR <valek37336@gmail.com>
2018-04-25 22:55:32 +0700

    Markup optimization

--- src/gtk/main_gtk.c
+++ src/gtk/main_gtk.c
@@ -4,35 +4,22 @@
 #include <glib.h>
 
 
-void on_learn_button_next_clicked();
-void on_retry_next_clicked();
-void on_retry_stop_clicked();
-void on_number_button_num_4_clicked();
-void on_number_button_num_3_clicked();
-void on_number_button_num_2_clicked();
-void on_number_button_num_1_clicked();
-void on_level_button_hight_clicked();
-void on_level_button_middle_clicked();
-void on_level_button_easy_clicked();
-void on_name_button_clicked();
-void on_hello_button_clicked();
-void on_menubar_about_about_activate();
-void on_menubar_profile_activate();
-void on_menubar_retry_activate();
-void on_menubar_learn__activate();
+void menubar_about_activate_cb();
+void menubar_profile_activate_cb();
 
 
-static GtkWidget* create_window(void) {
-    GtkWidget *window;
+
+static GtkWidget* create_windowgtk(void) {
+    GtkWidget *windowgtk;
     GtkBuilder *builder;
     GError* error = NULL;
 
     builder = gtk_builder_new();
-    if (!gtk_builder_add_from_file(builder, "./data/style/style.glade", &error)) {
+    if (!gtk_builder_add_from_file(builder, "./style/style.glade", &error)) {
         g_critical("Не могу загрузить файл: %s", error->message);
         g_error_free(error);
     }
-    GFile *file= g_file_new_for_path("./data/style/style.css");
+    GFile *file= g_file_new_for_path("./style/style.css");
     GtkCssProvider *provider = gtk_css_provider_new();
     if(!gtk_css_provider_load_from_file(provider, file, &error)) {
         g_warning("%s", error->message );
@@ -41,26 +28,26 @@ static GtkWidget* create_window(void) {
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
     gtk_builder_connect_signals(builder, NULL);
 
-    window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));
-    if (!window) {
+    windowgtk = GTK_WIDGET (gtk_builder_get_object (builder, "windowgtk"));
+    if (!windowgtk) {
         g_critical("Ошибка при получении виджета окна");
     }
     g_object_unref(builder);
 
-    return window;
+    return windowgtk;
 }
 
-static GtkWidget* create_dialog(void) {
-    GtkWidget *dialog;
+static GtkWidget* create_dialoggtk(void) {
+    GtkWidget *dialoggtk;
     GtkBuilder *builder;
     GError* error = NULL;
 
     builder = gtk_builder_new ();
-    if (!gtk_builder_add_from_file (builder, "./data/style/style.glade", &error)) {
+    if (!gtk_builder_add_from_file (builder, "./style/style.glade", &error)) {
         g_critical("Не могу загрузить файл: %s", error->message);
         g_error_free(error);
     }
-    GFile *file= g_file_new_for_path("./data/style/style.css");
+    GFile *file= g_file_new_for_path("./style/style.css");
     GtkCssProvider *provider = gtk_css_provider_new();
     if(!gtk_css_provider_load_from_file(provider, file, &error)) {
         g_warning("%s", error->message );
@@ -69,26 +56,26 @@ static GtkWidget* create_dialog(void) {
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
     gtk_builder_connect_signals (builder, NULL);
         
-    dialog = GTK_WIDGET(gtk_builder_get_object(builder, "dialog"));
-    if (!dialog) {
+    dialoggtk = GTK_WIDGET(gtk_builder_get_object(builder, "dialoggtk"));
+    if (!dialoggtk) {
         g_critical("Ошибка при получении виджета окна");
     }
     g_object_unref(builder);
 
-    return dialog;
+    return dialoggtk;
 }
 
-static GtkWidget* create_profile(void) {
-    GtkWidget *profile;
+static GtkWidget* create_profilegtk(void) {
+    GtkWidget *profilegtk;
     GtkBuilder *builder;
     GError* error = NULL;
 
     builder = gtk_builder_new ();
-    if (!gtk_builder_add_from_file(builder, "./data/style/style.glade", &error)) {
+    if (!gtk_builder_add_from_file(builder, "./style/style.glade", &error)) {
         g_critical("Не могу загрузить файл: %s", error->message);
         g_error_free(error);
     }
-    GFile *file= g_file_new_for_path("./data/style/style.css");
+    GFile *file= g_file_new_for_path("./style/style.css");
     GtkCssProvider *provider = gtk_css_provider_new();
     if(!gtk_css_provider_load_from_file(provider, file, &error)) {
         g_warning("%s", error->message);
@@ -97,25 +84,25 @@ static GtkWidget* create_profile(void) {
     gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
     gtk_builder_connect_signals(builder, NULL);
         
-    profile = GTK_WIDGET(gtk_builder_get_object (builder, "profile"));
-    if (!profile) {
+    profilegtk = GTK_WIDGET(gtk_builder_get_object (builder, "profilegtk"));
+    if (!profilegtk) {
         g_critical("Ошибка при получении виджета окна");
     }
     g_object_unref(builder);
 
-    return profile;
+    return profilegtk;
 }
 
-void main_dialog() {
-    GtkWidget *dialog;
-    dialog = create_dialog();
-    gtk_widget_show(dialog);
+void main_dialoggtk() {
+    GtkWidget *dialoggtk;
+    dialoggtk = create_dialoggtk();
+    gtk_widget_show(dialoggtk);
 }
 
-void main_window_profile() {
-    GtkWidget *profile;
-    profile = create_profile();
-    gtk_widget_show(profile);
+void main_window_profilegtk() {
+    GtkWidget *profilegtk;
+    profilegtk = create_profilegtk();
+    gtk_widget_show(profilegtk);
 }
 
 void on_window_header_delete_event() {
@@ -123,18 +110,18 @@ void on_window_header_delete_event() {
     gtk_main_quit();
 }
 
-void on_menubar_about_about_activate() {
-    main_dialog();
+void menubar_about_activate_cb() {
+    main_dialoggtk();
 }
-void on_menubar_profile_activate() {
-    main_window_profile();
+void menubar_profile_activate_cb() {
+    main_window_profilegtk();
 }
 
 int main (int argc, char *argv[]) {
-    GtkWidget *window;
+    GtkWidget *windowgtk;
     gtk_init(&argc, &argv);
-    window = create_window();
-    gtk_widget_show(window);
+    windowgtk = create_windowgtk();
+    gtk_widget_show(windowgtk);
     gtk_main();
     return 0;
 }
