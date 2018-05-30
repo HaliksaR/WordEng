0f91d54603db8080c5ae5e76d526dfe2af73cfff
HaliksaR <35256960+HaliksaR@users.noreply.github.com>
2018-05-31 02:54:25 +0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,28 +3,28 @@ BREAK='\033[m'
 LCYAN='\033[1;36m'
 OS=$(lsb_release -is)
 
-if [ "${OS}" == "Ubuntu" ] || [ "${OS}" = "Debian" ]; then 
+if [ "${OS}" == "Ubuntu" ] || [ "${OS}" == "Debian" ]; then 
     echo -e "${LCYAN}Install_lib_C${BREAK}"
     apt install gcc -qq
     apt install libc-dev -qq
     echo -e "${LCYAN}Install_GTK+3${BREAK}"
     apt install libgtk-3-dev -qq
     echo -e "${LCYAN}Go Install_WordEng!${BREAK}"
-elif [ "{$OS}" == "Solus" ]; then 
+elif [ "${OS}" == "Solus" ]; then 
     echo -e "${LCYAN}Install_lib_C${BREAK}"
     eopkg install gcc
-    eopkg  install libc-devel
+    eopkg  install glibc-devel
     echo -e "${LCYAN}Install_GTK+3${BREAK}"
     eopkg install libgtk-3-devel
     echo -e "${LCYAN}Go Install_WordEng!${BREAK}"
-elif [ "{$OS}" == "Arch" ] || [ "{$OS}" == "Arch linux" ]; then 
+elif [ "${OS}" == "Arch" ] || [ "${OS}" == "Arch linux" ]; then 
     echo -e "${LCYAN}Install_lib_C${BREAK}"
     pacman -S gcc
     pacman -S libc-devel
     echo -e "${LCYAN}Install_GTK+3${BREAK}"
     pacman -S gtk3
     echo -e "${LCYAN}Go Install_WordEng!${BREAK}"
-elif [ "{$OS}" == "RedHat" ] || [ "{$OS}" == "CentOS" ] || [ "{$OS}" == "Fedora" ]; then 
+elif [ "${OS}" == "RedHat" ] || [ "${OS}" == "CentOS" ] || [ "${OS}" == "Fedora" ]; then 
     echo -e "${LCYAN}Install_lib_C${BREAK}"
     yum install gcc
     yum install libc-devel
