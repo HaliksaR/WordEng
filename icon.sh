b32f641ba3f9be030ae0810699c3c2ffefcb7040
HaliksaR <valek37336@gmail.com>
2018-05-31 23:41:03 +0700

    Fix css fonts and icon.sh

--- icon.sh
+++ icon.sh
@@ -2,33 +2,18 @@
 BREAK='\033[m'
 LCYAN='\033[1;36m'
 current_dir=$(pwd)
+desktop_path=$(xdg-user-dir DESKTOP)
 
-for i in $(ls ~); do
-    if [ "${i}" == "Desktop" ]; then 
-        DESK=${i}
-        break
-    elif [ "${i}" == "Рабочий стол" ]; then
-        DESK=${i}
-        break
-    else 
-        DESK="0"
-    fi
-done
-
-if [ "${DESK}" != "0" ]; then 
-    echo "[Desktop Entry]
-    Version=1.0
-    Name=WordEng
-    Exec=$current_dir/bin/WordEng
-    Path=$current_dir/
-    Icon=$current_dir/data/style/img/logo.svg
-    Terminal=false
-    Type=Application
-    Categories=Gtk+3;Application;" > /home/$USER/${DESK}/WordEng.desktop
-    chmod +x /home/$USER/${DESK}/WordEng.desktop
-    echo -e "${LCYAN}|----------------------\\ ${BREAK}"
-    echo -e "${LCYAN}|Create icon on desktop |${BREAK}"
-    echo -e "${LCYAN}|----------------------/ ${BREAK}"
-else
-    echo -e "${LCYAN}I NOT FOUND DESKTOP${BREAK}"  
-fi
+echo "[Desktop Entry]
+Version=1.0
+Name=WordEng
+Exec=$current_dir/bin/WordEng
+Path=$current_dir/
+Icon=$current_dir/data/style/img/logo.svg
+Terminal=false
+Type=Application
+Categories=Gtk+3;Application;" > $desktop_path/WordEng.desktop
+chmod +x $desktop_path/WordEng.desktop
+echo -e "${LCYAN}|----------------------\\ ${BREAK}"
+echo -e "${LCYAN}|Create icon on desktop |${BREAK}"
+echo -e "${LCYAN}|----------------------/ ${BREAK}"
